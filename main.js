f630062f5f38e8185bf61ba3b49cecc5d0a23c79
Mathias Legarreta <mathiaslegarreta18@hotmail.com>
2024-01-09 18:42:04 -0300

    Primer Commit

---
+++ main.js
@@ -0,0 +1,52 @@
+class ProductManager {
+    static lastId = 0;
+
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(title, description, price, img, code, stock) {
+
+        if (!title || !description || !price || !img || !code || !stock) {
+            console.log("All fields are required.");
+            return;
+        }
+
+        if (this.products.some(item => item.code === code)) {
+            console.log("Code must be unique");
+            return;
+        }
+
+        const newProduct = {
+            id: ++ProductManager.lastId,
+            title,
+            description,
+            price,
+            img,
+            code,
+            stock
+
+        }
+
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(id) {
+        const product = this.products.find(item => item.id === id);
+
+        if (!product) {
+            console.log("Not Found");
+        } else {
+            console.log(product);
+
+        }
+
+
+    }
+
+
+
+}
